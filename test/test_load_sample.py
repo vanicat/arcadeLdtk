6347a229a9b3cda8b0a58bb6a3e42449cddee115
Rémi Vanicat <vanicat@debian.org>
2024-02-28 18:56:37 +0100

    Some more test

--- test/test_load_sample.py
+++ test/test_load_sample.py
@@ -2,6 +2,8 @@ import arcadeLDtk
 import arcade
 import os.path
 
+from arcadeLDtk import Level
+
 samples = [
     "AutoLayers_1_basic.ldtk",
     "AutoLayers_2_stamps.ldtk",
@@ -22,3 +24,40 @@ samples = [
 def test_load_sample():
     for path in samples:
         arcadeLDtk.read_LDtk(os.path.join("test/samples/", path))
+
+def test_many_features():
+    example = arcadeLDtk.read_LDtk("test/samples/Test_file_for_API_showing_all_features.ldtk")
+    assert isinstance(example.levels, list)
+    assert len(example.levels) == 4
+    assert isinstance(example.levels[0], Level)
+    fst_level = example.levels[0]
+    assert fst_level.identifier == "Everything"
+    scene = fst_level.make_scene()
+    for layer in fst_level.layers:
+        match layer.identifier:
+            case "Entities":
+                assert layer.type == "Entities"
+                assert layer.auto_layer_tiles is None
+                assert layer.grid_tiles is None
+                assert layer.int_grid_csv == []
+                for instance in layer.entity_list:
+                    if "target" in instance.fields:
+                        target = instance.fields["target"]
+                        assert isinstance(target.value, dict)
+                        example.get_entity(target.value) # type: ignore
+
+            case "IntGrid_8px_grid":
+                assert layer.type == "IntGrid"
+            case "PureAutoLayer":
+                assert layer.type == "AutoLayer"
+            case "IntGrid_without_rules":
+                assert layer.type == "IntGrid"
+                assert layer.has_tiles() is False
+            case "IntGrid_with_rules":
+                assert layer.type == "IntGrid"
+                assert layer.has_tiles()
+            case "Tiles":
+                assert layer.type == "Tiles"
+                assert layer.has_tiles()
+            case _:
+                assert False, f"unkonw layer: {layer.identifier}"
